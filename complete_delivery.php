e17eab4fd86e61feac064f0d6ba11c6193397159
Sohanuzzaman Soad <mdsoad@gmail.com>
2021-12-19 01:11:25 +0600

    Error Fix

--- complete_delivery.php
+++ complete_delivery.php
@@ -6,10 +6,24 @@
     }
 
     include "connect.php";
+    include "deliveryman_navbar.php";
 
     if (isset($_GET['deli_id'])) {
-    $sql0 = "UPDATE `delivery` SET job_status = 2 WHERE id=".$_GET['deli_id'];
-    $conn->query($sql0);
+        $sql = "SELECT * FROM `delivery` WHERE id=".$_GET['deli_id'];
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        $mobile = $row['mobile'];
+        $sql0 = "UPDATE `delivery` SET job_status = 2 WHERE id=".$_GET['deli_id'];
+        $conn->query($sql0);
+
+        $client->messages->create(
+            $mobile,  
+            [
+                'from' => $twilio_number,
+                "messagingServiceSid" => "MG0d50ed188ce17b75c054169370e5cedc",
+                'body' => 'Your money is successfully delivered'
+            ] 
+        );
     header("location:assigned_delivery.php");
     }
 
--- confirm_delivery.php
+++ confirm_delivery.php
@@ -1,36 +1,40 @@
 <?php
     /* Avoid multiple sessions warning
     Check if session is set before starting a new one. */
-require __DIR__ . '\library\twilio-php\src\Twilio\autoload.php';
-use Twilio\Rest\Client;
-// In production, these should be environment variables.
-$account_sid = 'ACf365d18367e7db2daee18446714fc6e0';
-$auth_token = '108846b736fb95458c7766498b5ff168';
-$twilio_number = "+14156809126"; // Twilio number you own
-$client = new Client($account_sid, $auth_token);
 // Below, substitute your cell phone
-$client->messages->create(
-    '+8801725683936',  
-    [
-        'from' => $twilio_number,
-        "messagingServiceSid" => "MG0d50ed188ce17b75c054169370e5cedc",
-        'body' => 'Delivery Confirmed'
-    ] 
-);
+if(!isset($_SESSION)) {
+    session_start();
+}
 
-    
-    
-    if(!isset($_SESSION)) {
-        session_start();
-    }
+include "connect.php";
 
-    include "connect.php";
+include "deliveryman_navbar.php";
+if (isset($_GET['deli_id'])) {
+    $sql = "SELECT * FROM `delivery` WHERE id=".$_GET['deli_id'];
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $mobile = $row['mobile'];
 
-    if (isset($_GET['deli_id'])) {
     $sql0 = "UPDATE `delivery` SET job_status = 1 WHERE id=".$_GET['deli_id'];
     $conn->query($sql0);
+    
+
+    // $client->messages->create(
+    //     $mobile,  
+    //     [
+    //         'from' => $twilio_number,
+    //         "messagingServiceSid" => "MG0d50ed188ce17b75c054169370e5cedc",
+    //         'body' => 'Delivery Confirmed'
+    //     ] 
+    // );
     header("location:assigned_delivery.php");
+
     }
+
+
+    
+
+    
 
 
 
