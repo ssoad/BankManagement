c5a66006aa992cd31508549aeda61c32c226619f
Sohanuzzaman Soad <mdsoad@gmail.com>
2021-12-19 01:49:17 +0600

    update

--- request_delivery.php
+++ request_delivery.php
@@ -12,30 +12,44 @@ $trans_id = $_GET['trans_id'];
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="action_style.css">
+    <link rel="stylesheet" href="customer_add_style.css">
 </head>
 
 <body>
             
     <div class="flex-container">
         <div class="flex-item">
-        <form style="padding: 20px 20px 20px 20px" action="place_delivery.php?trans_id=<?php echo "$trans_id";?>" method="post">
-                    <div class="flex-item-login">
-                        <h4>Enter Adress and Contact Info</h2>
-                    </div>
+        
+        <form class="add_customer_form" action="place_delivery.php?trans_id=<?php echo "$trans_id";?>" method="post">
+        <div class="flex-container-form_header">
+            <h1 id="form_header">Please fill in the following details</h1>
+        </div>
 
-                    <div class="flex-item">
-                        <input type="text" name="address" placeholder="Enter your Address" required>
-                    </div>
+        <div class="flex-container">
+            <div class=container>
+                <label>Address :</label><br>
+                <textarea name="address" required /></textarea>
+            </div>
+        </div>
 
-                    <div class="flex-item">
-                        <input type="text" name="mobile" placeholder="Enter your Mobile" required>
-                    </div>
+        <div class="flex-container">
+            
+            <div  class=container>
+                <label>Phone No. :</b></label><br>
+                <input name="phno" size="30" type="text" required />
+            </div>
+        </div>
 
-                    <div class="flex-item">
-                        <button type="submit">Submit</button>
-                    </div>
-            </form>
+        
+        <div class="flex-container">
+            <div class="container">
+                <button type="submit">Submit</button>
+            </div>
+
+        </div>
+
+    </form>
+
         </div>
     </div>
 
